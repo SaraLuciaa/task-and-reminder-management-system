cf602710b538968d7481b9066358ed2ef6ef8dbf
SaraLuciaa <saludipu71@gmail.com>
2023-10-01 17:08:53 -0500

    Intellij Configuration with JavaFX - Version 15

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,22 @@
-public class Main {
+package ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("task-reminder.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch();
     }
 }
