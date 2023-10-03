8bc601fa09237cd8f8c5c8e0c0c82ca6ecf3bfa1
SaraLuciaa <saludipu71@gmail.com>
2023-10-02 21:31:17 -0500

    In this update the 'New activity' and 'Go back' buttons work.

--- src/ui/Activity.java
+++ src/ui/Activity.java
@@ -1,12 +1,18 @@
 package ui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Activity {
 
@@ -35,8 +41,11 @@ public class Activity {
     private TextField title;
 
     @FXML
-    void goHome(MouseEvent event) {
-
+    void goHome(MouseEvent event) throws IOException {
+        Stage stage = (Stage) goBack.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("task-reminder.fxml"));
+        stage.setTitle("Task & Reminder Management :P");
+        stage.setScene(new Scene(root));
     }
 
     @FXML
--- src/ui/Home.java
+++ src/ui/Home.java
@@ -1,8 +1,14 @@
 package ui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Home {
 
@@ -10,7 +16,10 @@ public class Home {
     private Button addActivity;
 
     @FXML
-    void goToNewActivity(MouseEvent event) {
-        System.out.println(":P");
+    void goToNewActivity(MouseEvent event) throws IOException {
+        Stage stage = (Stage) addActivity.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("new-activity.fxml"));
+        stage.setTitle("Task & Reminder Management :P");
+        stage.setScene(new Scene(root));
     }
 }
